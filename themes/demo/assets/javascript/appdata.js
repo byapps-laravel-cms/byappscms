81c550c17d11224b36ed94303072ac155c26ea18
origin <jipark0716@gmail.com>
2019-05-28 16:43:53 +0900

    만료예정 업체 표시

--- themes/demo/assets/javascript/appdata.js
+++ themes/demo/assets/javascript/appdata.js
@@ -20,4 +20,16 @@ $(document).ready(function (){
         $(".aside").fadeOut(500);
         $(".container").css({"margin": "0 auto"});
     })
+    
+    //home 만료예정업체
+    $(".app_box_cover").eq(0).css({"display":"block"}); 
+    $(".app_select li").eq(0).css({"background-color":"#c2d4ef"});
+    //click
+    $(".app_select li").click(function (){
+        var index = $(".app_select li").index(this);
+        $(".app_box_cover").css({"display":"none"});
+        $(".app_select li").css({"background-color":"#fff"});
+        $(".app_box_cover").eq(index).css({"display":"block"}); 
+        $(".app_select li").eq(index).css({"background-color":"#c2d4ef"});
+    })
 })
