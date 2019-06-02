a3623dc97af1a9d8c4ec5ca4d60718edd5b0cabe
2jiwon <ljwjulian@gmail.com>
2019-06-02 22:16:17 +0900

    UpdateManage 모델파일 수정

--- plugins/jiwon/byapps/models/UpdateManage.php
+++ plugins/jiwon/byapps/models/UpdateManage.php
@@ -15,31 +15,12 @@ class UpdateManage extends Model
      */
     public $timestamps = false;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    // 다른 DB불러오기
->>>>>>> 46b6c03aff7f11902c7fecd9a98e029d8e45fd80
-=======
-    // 다른 DB불러오기
-=======
->>>>>>> develop
->>>>>>> a454bcef5dcf7867c674f2b49313e3278579304b
     public $connection = 'byapps';
 
     /**
      * @var string The database table used by the model.
      */
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    //public $table = 'jiwon_byapps_apk_data';
->>>>>>> 46b6c03aff7f11902c7fecd9a98e029d8e45fd80
-=======
-    //public $table = 'jiwon_byapps_apk_data';
-=======
->>>>>>> develop
->>>>>>> a454bcef5dcf7867c674f2b49313e3278579304b
+
     public $table = 'BYAPPS_apps_update_data';
 
     /**
@@ -47,7 +28,6 @@ class UpdateManage extends Model
      */
     public $rules = [
     ];
-<<<<<<< HEAD
 
     public $hasMany = [
       'joins' => [
@@ -55,6 +35,4 @@ class UpdateManage extends Model
         'key' => 'idx'
       ]
     ];
-=======
->>>>>>> develop
 }
