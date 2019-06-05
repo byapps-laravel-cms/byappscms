c2e196b214b43c75b10a53d2e69315c25bbef53e
origin <jipark0716@gmail.com>
2019-06-05 10:37:08 +0900

    js 코딩규약에 맞게 수정

--- themes/demo/assets/javascript/app.js
+++ themes/demo/assets/javascript/app.js
@@ -23,123 +23,129 @@ function view_user (){
  *파일 업로드
 */
 
-function fileDropDown (dropZone)
-{
-    //Drag기능
-    dropZone.on('dragleave',function (e){
-        e.stopPropagation();
-        e.preventDefault();
-        // 드롭다운 영역 css
-        $(this).css({'background-color':'#fff','background':'','box-shadow': ''});
+function fileDropDown (dropZone){
+  //Drag기능
+  dropZone.on('dragleave',function (e){
+    e.stopPropagation();
+    e.preventDefault();
+    // 드롭다운 영역 css
+    $(this).css({'background-color':'#fff','background':'','box-shadow': ''});
+  });
+  dropZone.on('dragover',function (e){
+    e.stopPropagation();
+    e.preventDefault();
+    // 드롭다운 영역 css
+    $(this).css({
+      "background":"url(https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-cloud-upload-outline-256.png)",
+      'background-repeat':'no-repeat',
+      'background-position':'center',
+      'box-shadow':'inset 0 0 8px 1px black'
     });
-    dropZone.on('dragover',function (e){
-        e.stopPropagation();
-        e.preventDefault();
-        // 드롭다운 영역 css
-        $(this).css({"background":"url(https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-cloud-upload-outline-256.png)", 'background-repeat' : 'no-repeat', 'background-position':'center','box-shadow': 'inset 0 0 8px 1px black'});
+  });
+  dropZone.on('drop',function (e){
+    e.preventDefault();
+    // 드롭다운 영역 css
+    $(this).css({
+      'background':'url(https://cdn1.iconfinder.com/data/icons/hawcons/32/698830-icon-104-folder-checked-256.png)',
+      'background-repeat':'no-repeat',
+      'background-position':'center',
+      'box-shadow':''
     });
-    dropZone.on('drop',function (e){
-        e.preventDefault();
-        // 드롭다운 영역 css
-        $(this).css({'background':'url(https://cdn1.iconfinder.com/data/icons/hawcons/32/698830-icon-104-folder-checked-256.png)','background-repeat' : 'no-repeat', 'background-position':'center','box-shadow': ''});
 
-        var files = e.originalEvent.dataTransfer.files;
-        if(files != null){
-            if(files.length < 1){
-                alert("폴더 업로드 불가");
-                return;
-            }
-            selectFile(files,$(this))
-        }else{
-            alert("ERROR");
-        }
-    });
+  var files = e.originalEvent.dataTransfer.files;
+    if(files != null){
+      if(files.length < 1){
+        alert("폴더 업로드 불가");
+        return;
+      }
+      selectFile(files,$(this))
+    }else{
+      alert("ERROR");
+      }
+  });
 }
 
 function selectFile (fileObject,target){
-    var files = null;
+  var files = null;
 
-    if(fileObject != null){
-        files = fileObject;
-    }else{
-        files = $('#multipaartFileList_' + fileIndex)[0].files;
-    }
-    addFileList(files[0],target);
+  if(fileObject != null){
+    files = fileObject;
+  }else{
+    files = $('#multipaartFileList_' + fileIndex)[0].files;
+  }
+  addFileList(files[0],target);
 }
 function addFileList (file,target){
-    formData = new FormData($('form').eq(0));
-    formData.append('file',file);
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('[name="_token"]').attr('value')
-        }
-    });
-    $.ajax({
-        url:'./',
-        data:formData,
-        type:'POST',
-        enctype:'multipart/form-data',
-        processData:false,
-        contentType:false,
-        dataType:'json',
-        cache:false,
-        success : function (data) {
-            target.css('background-image',`url('${data}')`)
-            target.text('');
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            target.css('background-image',`url('${jqXHR.responseText}')`);
-            target.text('');
-        }
-    });
+  formData = new FormData($('form').eq(0));
+  formData.append('file',file);
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-TOKEN': $('[name="_token"]').attr('value')
+    }
+  });
+  $.ajax({
+    url:'./',
+    data:formData,
+    type:'POST',
+    enctype:'multipart/form-data',
+    processData:false,
+    contentType:false,
+    dataType:'json',
+    cache:false,
+    success : function (data) {
+      target.css('background-image',`url('${data}')`)
+      target.text('');
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      target.css('background-image',`url('${jqXHR.responseText}')`);
+      target.text('');
+    }
+  });
 }
 /*
  * Application
  */
 $(document).tooltip({
-    selector: "[data-toggle=tooltip]"
+  selector: "[data-toggle=tooltip]"
 })
 /*
  * Auto hide navbar
  */
 jQuery(document).ready(function($){
-    var $header = $('.navbar-autohide'),
-        scrolling = false,
-        previousTop = 0,
-        currentTop = 0,
-        scrollDelta = 10,
-        scrollOffset = 150
+  var $header = $('.navbar-autohide'),
+    scrolling = false,
+    previousTop = 0,
+    currentTop = 0,
+    scrollDelta = 10,
+    scrollOffset = 150
 
-    $(window).on('scroll', function(){
-        if (!scrolling) {
-            scrolling = true
+  $(window).on('scroll', function(){
+    if (!scrolling) {
+      scrolling = true
+      if (!window.requestAnimationFrame) {
+        setTimeout(autoHideHeader, 250)
+      }else {
+        requestAnimationFrame(autoHideHeader)
+      }
+    }
+  })
 
-            if (!window.requestAnimationFrame) {
-                setTimeout(autoHideHeader, 250)
-            }
-            else {
-                requestAnimationFrame(autoHideHeader)
-            }
-        }
-    })
-
-    function autoHideHeader () {
-        var currentTop = $(window).scrollTop()
-        // Scrolling up
-        if (previousTop - currentTop > scrollDelta) {
-            $header.removeClass('is-hidden');
-            $("#sidebar, #sidebar-toggle").css({"top":$(".navbar-header").height()});
-        }
-        else if (currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
-            // Scrolling down
-            $header.addClass('is-hidden');
-            $("#sidebar, #sidebar-toggle").css({"top":'0'});
-        } else if ( currentTop > 100 ) {
-            $( '.top_btn' ).fadeIn();
-        } else {
-            $( '.top_btn' ).fadeOut();
-        }
-        previousTop = currentTop
-        scrolling = false
+  function autoHideHeader () {
+    var currentTop = $(window).scrollTop()
+    // Scrolling up
+    if (previousTop - currentTop > scrollDelta) {
+      $header.removeClass('is-hidden');
+      $("#sidebar, #sidebar-toggle").css({"top":$(".navbar-header").height()});
+    }else if (currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+      // Scrolling down
+      $header.addClass('is-hidden');
+      $("#sidebar, #sidebar-toggle").css({"top":'0'});
+    }else if ( currentTop > 100 ) {
+      $( '.top_btn' ).fadeIn();
+    } else {
+      $( '.top_btn' ).fadeOut();
+    }
+      previousTop = currentTop
+      scrolling = false
     }
 });
--- themes/demo/assets/javascript/appdata.js
+++ themes/demo/assets/javascript/appdata.js
@@ -1,19 +1,19 @@
 //앱 상세 더보기
 $(".show_btn").click(function (){
-    if($(".top_banner").css("display")=="block"){
-        $(".top_banner").slideUp(500);
-        $(".show_btn").css({"background-color":"#c6c6c6"});
-    }else{
-        $(".top_banner").slideDown(500); 
-        $(".show_btn").css({"background-color":"#d9edf7"});   
-    }
+  if($(".top_banner").css("display")=="block"){
+    $(".top_banner").slideUp(500);
+    $(".show_btn").css({"background-color":"#c6c6c6"});
+  }else{
+    $(".top_banner").slideDown(500); 
+    $(".show_btn").css({"background-color":"#d9edf7"});   
+  }
 });
 //카테고리 변경 확인
 if(Util.getCookie('searchType')){
-    $('.select_comm_1 option').eq(Util.getCookie('searchType')*1).attr('selected','selected');
+  $('.select_comm_1 option').eq(Util.getCookie('searchType')*1).attr('selected','selected');
 }
 $('.select_comm_1').change(function () {
-    document.cookie = `searchType=${$('.select_comm_1 option:selected').index()};path=/`;
+  document.cookie = `searchType=${$('.select_comm_1 option:selected').index()};path=/`;
 });
 //쿠키
 if(Util.getCookie('aside') == 'on' && $('#sidebar').length != 0){
@@ -24,38 +24,54 @@ $(".app_box_cover").eq(0).css({"display":"block"});
 $(".app_select li").eq(0).css({"background-color":"#c2d4ef"});
 //click
 $(".app_select li").click(function (){
-    var index = $(".app_select li").index(this);
-    $(".app_box_cover").css({"display":"none"});
-    $(".app_select li").css({"background-color":"#c6c6c6"});
-    $(".app_box_cover").eq(index).css({"display":"block"}); 
-    $(".app_select li").eq(index).css({"background-color":"#c2d4ef"});
+  var index = $(".app_select li").index(this);
+  $(".app_box_cover").css({"display":"none"});
+  $(".app_select li").css({"background-color":"#c6c6c6"});
+  $(".app_box_cover").eq(index).css({"display":"block"}); 
+  $(".app_select li").eq(index).css({"background-color":"#c2d4ef"});
 });
 //앱 상세 첫 ㅍ시 화면
 $(".develop_info_select li").eq(0).css({"background-color":"#c2d4ef"});
 $(".develop_info").eq(0).css({"display":"block"});
 //click
 $(".develop_info_select li").click(function (){
-    var index = $(".develop_info_select li").index(this);
-    //초기화
-    $(".develop_info_select li").css({"background-color":"#c6c6c6"});
-    $(".develop_info").css({"display":"none"});
-    //변경
-    $(".develop_info_select li").eq(index).css({"background-color":"#c2d4ef"});
-    $(".develop_info").eq(index).css({"display":"block"});
+  var index = $(".develop_info_select li").index(this);
+  //초기화
+  $(".develop_info_select li").css({"background-color":"#c6c6c6"});
+  $(".develop_info").css({"display":"none"});
+  //변경
+  $(".develop_info_select li").eq(index).css({"background-color":"#c2d4ef"});
+  $(".develop_info").eq(index).css({"display":"block"});
 });
 //header로 가는 버튼
 $('.top_btn').click( function () {
-      $( 'html, body' ).animate( { scrollTop : 0 }, 400 );
-      return false;
-} );
+  $( 'html, body' ).animate( { scrollTop : 0 }, 400 );
+  return false;
+});
+
+
+
+function platform_chk (){
+  var filter = "win16|win32|win64|mac|macintel"; 
+  if ( navigator.platform ) {
+    if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) { 
+      $("#slidebar").css({"width":"58%"});
+      return "mobile";
+    } else { 
+      $("#slidebar").css({"width":"41%"});
+      return "pc";
+    }
+  }    
+}
 
 $("#sidebar-close,#sidebar-toggle").click(function (){
   $("html").toggleClass("open");
+  platform_chk();
   if($("html").attr('class')=="open"){
-      $(".container").css({"margin":"0"});
-     document.cookie = "aside=on;path=/";
-    }else{
-        $(".container").css({"margin":""});
-        document.cookie = "aside=off;path=/";   
-    }
+  $(".container").css({"margin":"0"});
+    document.cookie = "aside=on;path=/";
+  }else{
+    $(".container").css({"margin":""});
+    document.cookie = "aside=off;path=/";   
+  }
 });
