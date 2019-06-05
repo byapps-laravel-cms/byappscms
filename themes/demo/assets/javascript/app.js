863a62aeee4d778920d05c9a8750df650af29696
origin <jipark0716@gmail.com>
2019-06-05 09:39:23 +0900

    chart ajax로 데이터 요청 및 셈플 데이터 입력

--- themes/demo/assets/javascript/app.js
+++ themes/demo/assets/javascript/app.js
@@ -1,11 +1,97 @@
+$(function (){
+    fileDropDown($(".file_dropzone"));
+});
+/*
+ * 긴 이름 자르기
+*/
+
+var tdCount = $('#example tr:first-child td').length;
+for(var i = 0 ; i < $('#example td').length ; i += tdCount){
+    var item = $('#example td a').eq(i);
+    var temp = item.text().trim();
+    if(temp.length > 30){
+        item.text(`${temp.substr(0,28)}...`);
+    }
+}
+/*
+ *파일 업로드
+*/
+
+function fileDropDown(dropZone){
+    //Drag기능
+    dropZone.on('dragleave',function(e){
+        e.stopPropagation();
+        e.preventDefault();
+        // 드롭다운 영역 css
+        $(this).css({'background-color':'#fff','background':'','box-shadow': ''});
+    });
+    dropZone.on('dragover',function(e){
+        e.stopPropagation();
+        e.preventDefault();
+        // 드롭다운 영역 css
+        $(this).css({"background":"url(https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-cloud-upload-outline-256.png)", 'background-repeat' : 'no-repeat', 'background-position':'center','box-shadow': 'inset 0 0 8px 1px black'});
+    });
+    dropZone.on('drop',function(e){
+        e.preventDefault();
+        // 드롭다운 영역 css
+        $(this).css({'background':'url(https://cdn1.iconfinder.com/data/icons/hawcons/32/698830-icon-104-folder-checked-256.png)','background-repeat' : 'no-repeat', 'background-position':'center','box-shadow': ''});
+
+        var files = e.originalEvent.dataTransfer.files;
+        if(files != null){
+            if(files.length < 1){
+                alert("폴더 업로드 불가");
+                return;
+            }
+            selectFile(files,$(this))
+        }else{
+            alert("ERROR");
+        }
+    });
+}
+
+function selectFile(fileObject,target){
+    var files = null;
+
+    if(fileObject != null){
+        files = fileObject;
+    }else{
+        files = $('#multipaartFileList_' + fileIndex)[0].files;
+    }
+    addFileList(files[0],target);
+}
+function addFileList(file,target){
+    formData = new FormData($('form').eq(0));
+    formData.append('file',file);
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('[name="_token"]').attr('value')
+        }
+    });
+    $.ajax({
+        url:'./',
+        data:formData,
+        type:'POST',
+        enctype:'multipart/form-data',
+        processData:false,
+        contentType:false,
+        dataType:'json',
+        cache:false,
+        success : function(data) {
+            target.css('background-image',`url('${data}')`)
+            target.text('');
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            target.css('background-image',`url('${jqXHR.responseText}')`);
+            target.text('');
+        }
+    });
+}
 /*
  * Application
  */
-
 $(document).tooltip({
     selector: "[data-toggle=tooltip]"
 })
-
 /*
  * Auto hide navbar
  */
--- themes/demo/assets/javascript/temp.js
+++
@@ -1,9 +0,0 @@
-var asdf;
-var tdCount = $('#example tr:first-child td').length;
-for(var i = 0 ; i < $('#example td').length ; i += tdCount){
-    var item = $('#example td a').eq(i);
-    var temp = item.text().trim();
-    if(temp.length > 30){
-        item.text(`${temp.substr(0,28)}...`);
-    }
-}
