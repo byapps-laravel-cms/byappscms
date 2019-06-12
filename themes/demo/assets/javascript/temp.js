c7e23a0a0b080363e8f98a8f61faa28a5a3da2d2
origin <jipark0716@gmail.com>
2019-06-12 16:35:40 +0900

    만료 예정업체 만료된 업체 다르게 표시

--- themes/demo/assets/javascript/temp.js
+++ themes/demo/assets/javascript/temp.js
@@ -12,14 +12,14 @@ function asdf(){
     })
     return null;
 }
-var li1 = "<div class='col-sm-1 col-xs-3'><img class='h-25 p-10 d-inline-block'  src='https://cms.byapps.co.kr/space/ic_launcher.png' alt='#'><p>2019-06-03</p></div>"
-var li2 = "<div class='col-sm-1 col-xs-3'><img class='h-25 p-10 d-inline-block'  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd20d5uTTvOJ6kT7ANcGPb3iNnOcMfTDAtEVUrvTlOrRrzyTuE5w' alt='#'><p>2019-06-03</p></div>"
-var li3 = "<div class='col-sm-1 col-xs-3'><img class='h-25 p-10 d-inline-block'  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStI_5Isvkt5A8ERpkETaf63WbicSbtpeAEmbhp4SuM9WkX6Fa7' alt='#'><p>2019-06-03</p></div>"
-for(var i=0; i<40; i++){
-    $("#layout3 > .dragbox > .row:nth-child(2)").append(li1);
-    $("#layout3 > .dragbox > .row:nth-child(3)").append(li2);
-    $("#layout3 > .dragbox > .row:nth-child(4)").append(li3);
-}
+// var li1 = "<div class='col-sm-1 col-xs-3'><img class='h-25 p-10 d-inline-block'  src='https://cms.byapps.co.kr/space/ic_launcher.png' alt='#'><p>2019-06-03</p></div>"
+// var li2 = "<div class='col-sm-1 col-xs-3'><img class='h-25 p-10 d-inline-block'  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd20d5uTTvOJ6kT7ANcGPb3iNnOcMfTDAtEVUrvTlOrRrzyTuE5w' alt='#'><p>2019-06-03</p></div>"
+// var li3 = "<div class='col-sm-1 col-xs-3'><img class='h-25 p-10 d-inline-block'  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStI_5Isvkt5A8ERpkETaf63WbicSbtpeAEmbhp4SuM9WkX6Fa7' alt='#'><p>2019-06-03</p></div>"
+// for(var i=0; i<40; i++){
+//     $("#layout3 > .dragbox > .row:nth-child(2)").append(li1);
+//     $("#layout3 > .dragbox > .row:nth-child(3)").append(li2);
+//     $("#layout3 > .dragbox > .row:nth-child(4)").append(li3);
+// }
 
 $("#layout3 > .dragbox > .row:not(:first-child").css({'display':'none'});
 $("#layout3 > .dragbox > .row:nth-child(2)").css({'display':'block'});
