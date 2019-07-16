5e10d644e3f642b049acad6d2eec2067da6351e4
2jiwon <ljwjulian@gmail.com>
2019-07-16 14:55:10 +0900

    pusher-js, laravel-echo 설치

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -30,11 +30,12 @@ return [
 
         'pusher' => [
             'driver' => 'pusher',
-            'key' => '',
-            'secret' => '',
-            'app_id' => '',
+            'key' => env('PUSHER_APP_KEY', ''),
+            'secret' => env('PUSHER_APP_SECRET', ''),
+            'app_id' => env('PUSHER_APP_ID', ''),
             'options' => [
-                //
+                'cluster' => env('PUSHER_APP_CLUSTER', ''),
+                'encrypted' => true
             ],
         ],
 
