ba2a3955972b26b810fbbf12b51c4a4612d3b224
2jiwon <ljwjulian@gmail.com>
2019-07-16 14:37:41 +0900

    comment 컴포넌트에 옵션 추가

--- plugins/jiwon/byapps/components/Comments.php
+++ plugins/jiwon/byapps/components/Comments.php
@@ -17,6 +17,13 @@ class Comments extends ComponentBase
 {
   public $comments;
 
+  // public $mmid = [
+  //   'apps', 'csqna', 'design_order', 'free_order',
+  //   'ma', 'member', 'myqna', 'new_update',
+  //   'order', 'payment', 'promotion', 'retaku', 'reseller',
+  //   'work'
+  // ];
+
   public function componentDetails()
   {
       return [
@@ -35,8 +42,35 @@ class Comments extends ComponentBase
               'validationPattern' => '^[0-9]+$',
               'validationMessage' => '숫자만 적어주세요'
           ],
+
+          'target' => [
+              'title'       => '대상 테이블',
+              'description' => '코멘트 대상 테이블',
+              'type'        => 'dropdown',
+              'default'     => 'apps'
+          ]
       ];
   }
+
+  public function getTargetOptions() {
+      return [
+        'apps' => '앱 접수',
+        'csqna'=> '고객 문의',
+        'design_order' => '',
+        'free_order' => '',
+        'ma' => '부가서비스 접수',
+        'member' => '회원 문의',
+        'myqna' => '고객 상담',
+        'new_update' => '업데이트 관리',
+        'order' => '기타 서비스',
+        'payment' => '결제 관리',
+        'promotion' => '프로모션',
+        'retaku' => '',
+        'reseller' => '리셀러 정보',
+        'work' => ''
+      ];
+  }
+
 
   public function onRun()
   {
@@ -49,7 +83,13 @@ class Comments extends ComponentBase
 
      if ($this->property('display') > 0) {
        $query = $query->take($this->property('display'));
+
+       // if ($this->property('target') != '') {
+       //   $query = $query->where('mmid', $this->property('mmid'))->take($this->property('display'));
+       // }
      }
+
+
 
      //return Comment::all()->take(10)->sortBy('idx');
      return $query;
