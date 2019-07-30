4c5d7629da1af125e74602508397f6d21bb67fab
2jiwon <ljwjulian@gmail.com>
2019-07-30 14:56:38 +0900

    빈 컴포넌트 추가(최근리스트 및 검색폼)

---
+++ plugins/jiwon/byapps/components/RecentList.php
@@ -0,0 +1,19 @@
+<?php namespace \Jiwon/Byapps\Components;
+
+use Cms\Classes\ComponentBase;
+
+class RecentList extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'recentList Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
---
+++ plugins/jiwon/byapps/components/SearchForm.php
@@ -0,0 +1,19 @@
+<?php namespace \Jiwon/Byapps\Components;
+
+use Cms\Classes\ComponentBase;
+
+class SearchForm extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'SearchForm Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
