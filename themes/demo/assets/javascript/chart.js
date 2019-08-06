5ffc2d5a73431bc0d30a10d2270847600179d025
minbbal <jashely@naver.com>
2019-08-06 10:58:47 +0900

    차트 임의 데이터 추가

--- themes/demo/assets/javascript/chart.js
+++ themes/demo/assets/javascript/chart.js
@@ -6,23 +6,23 @@ $.request('onGetChartData', {
 });
 
 function showChart (data) {
-  var chart = bb.generate({
+    var chart = bb.generate({
     data: {
-      columns: data.circle1,
-      type: "donut",
-      colors: {
+        columns: data.circle1,
+        type: "donut",
+        colors: {
         "무료": "#17b4dd",
         "유료": "#038db2",
         "관리": "69bbd1"
-      },
+        },
     },
     donut: {
-      title: "0000"
+        title: "0000"
     },
-      bindto: "#ma_stats"
+        bindto: "#ma_stats"
     });
-    
-  var chart = bb.generate({
+
+    var chart = bb.generate({
     data: {
     columns: data.circle2,
     type: "donut",
@@ -37,24 +37,41 @@ function showChart (data) {
     },
     bindto: "#app_stats"
     });
-    
-  var chart = bb.generate({
+
+    var chart = bb.generate({
     data: {
-      columns: data.bar,
-      type: "bar",
-      colors: {
+        columns: data.bar,
+        type: "bar",
+        colors: {
         "신규": "#fca1b0",
         "연장": "#f9637c",
         "기타": "#d7215c"
-      }
+        }
     },
     bar: {
-      width: {
+        width: {
         ratio: 0.8
-      }
+        }
     },
-      bindto: "#sale_stats"
+        bindto: "#sale_stats"
     });
 
+    // var chart = bb.generate({
+    //     data: {
+    //         columns: data.bar,
+    //         type: "bar",
+    //         colors: {
+    //         "앱 접수": "#fca1b0",
+    //         "연장": "#f9637c",
+    //         "기타": "#d7215c"
+    //         }
+    //     },
+    //     bar: {
+    //         width: {
+    //         ratio: 0.8
+    //         }
+    //     },
+    //         bindto: "#take_order"
+    //     });
     
 }
