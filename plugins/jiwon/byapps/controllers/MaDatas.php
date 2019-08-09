298ee0f39689b855a565d437ea0647147c91a880
2jiwon <ljwjulian@gmail.com>
2019-08-09 20:10:41 +0900

    MA이용업체 model명: MaData. 기존 MAService 정리

--- plugins/jiwon/byapps/controllers/MaDatas.php
+++ plugins/jiwon/byapps/controllers/MaDatas.php
@@ -3,10 +3,10 @@
 use Backend\Classes\Controller;
 use BackendMenu;
 
-class MAServices extends Controller
+class MaDatas extends Controller
 {
     public $implement = [        'Backend\Behaviors\ListController'    ];
-    
+
     public $listConfig = 'config_list.yaml';
 
     public function __construct()
