4b0cecf33b6540ee3815da250e4e2270ed088300
2jiwon <ljwjulian@gmail.com>
2019-08-27 15:51:35 +0900

    만료 예정업체 탭 부분 재 작업

--- plugins/jiwon/byapps/components/ExpiredData.php
+++ plugins/jiwon/byapps/components/ExpiredData.php
@@ -19,23 +19,38 @@ class ExpiredData extends ComponentBase
     }
 
 
+    // 만료된 업체들
     public function getExpiredApps()
     {
       // iOS 계정만료 예정업체
       // original query: select app_id,app_name,ios_dev_exp from BYAPPS_apps_data where ios_dev_exp!='' and ios_dev_exp<'".$next_month."' order by ios_dev_exp
       //  select app_id,app_name,ios_dev_exp from BYAPPS_apps_data where ios_dev_exp!='' and ios_dev_exp < '2019-08-01' order by ios_dev_exp
       $next_month=date("Y-m-d",strtotime("now +30 day"));
+    	$todate=date("Y-m-d");
 
       return AppsData::select('app_name', 'app_id', 'ios_dev_exp')
-                          ->where('ios_dev_exp', '!=', '')
-                          ->where('ios_dev_exp', '<', $next_month)
-                          ->orderBy('ios_dev_exp')
-                          ->get();
+              ->where('ios_dev_exp', '!=', '')
+              ->where('ios_dev_exp', '<', $todate)
+              ->orderBy('ios_dev_exp', 'asc')
+              ->get();
+    }
 
+    // 만료예정 업체들
+    public function getWillBeExpiredApps()
+    {
+      $next_month=date("Y-m-d",strtotime("now +30 day"));
+      $todate=date("Y-m-d",strtotime("now +1 day"));
+
+      return AppsData::select('app_name', 'app_id', 'ios_dev_exp')
+                        ->where('ios_dev_exp', '!=', '')
+                        ->whereBetween('ios_dev_exp', [$todate, $next_month])
+                        ->orderBy('ios_dev_exp', 'asc')
+                        ->get();
     }
 
     public function onRun()
     {
-      $this->page['appsData'] = $this->getExpiredApps();
+      $this->page['expiredApps'] = $this->getExpiredApps();
+      $this->page['willBeExpiredApps'] = $this->getWillBeExpiredApps();
     }
 }
